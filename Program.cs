21c7d72da660a38b2af99ae0a672ce0ea10464a2
Dan Lumpp <20948500+dlumpp@users.noreply.github.com>
2020-10-30 14:25:48 -0400

    create console app

---
+++ Program.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.Security;
+using System.Net.Sockets;
+
+namespace SSLHelper
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            if (args.Length == 0)
+            {
+                Console.WriteLine("Usage: SSLHelper [hostname] [port]");
+                Console.WriteLine("port defaults to 443 if not specified");
+                return;
+            }
+
+            string hostname = args[0];
+            int port = args.Length == 1 ? 443 : int.Parse(args[1]);
+
+            var addr = Dns.GetHostAddresses(hostname).First();
+            Console.WriteLine($"DNS resolved {hostname} to address {addr}");
+            IPEndPoint remoteEP = new IPEndPoint(addr, port);
+
+            Console.WriteLine($"Creating TCP Socket and NetworkStream");
+            Socket s;
+            Stream tcpStream;
+            try
+            {
+                s = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+                s.Connect(remoteEP);
+                tcpStream = new NetworkStream(s, ownsSocket: true);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine($"Network Stream creation failed with {e.GetType()}: {e.ToString()}");
+                return;
+            }
+
+            using (SslStream sslStream = new SslStream(tcpStream, false, SSLHelper.OnRemoteCertificateReceived, SSLHelper.OnClientCertificateRequested, EncryptionPolicy.RequireEncryption))
+            {
+                try
+                {
+                    sslStream.AuthenticateAsClient(hostname);
+
+                    Console.WriteLine($"TransportContext: {sslStream.TransportContext}");
+                    Console.WriteLine($"Protocol: {sslStream.SslProtocol}");
+
+                    Console.WriteLine($"KeyExchangeAlgorithm: {sslStream.KeyExchangeAlgorithm}");
+                    Console.WriteLine($"KeyExchangeStrength: {sslStream.KeyExchangeStrength}");
+
+                    Console.WriteLine($"CipherAlgorithm: {sslStream.CipherAlgorithm}");
+                    Console.WriteLine($"CipherStrength: {sslStream.CipherStrength}");
+
+                    Console.WriteLine($"HashAlgorithm: {sslStream.HashAlgorithm}");
+                    Console.WriteLine($"HashStrength: {sslStream.HashStrength}");
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine($"SSL Stream creation failed with {e.GetType()}: {e.ToString()}");
+                    return;
+                }
+            }
+
+            Console.WriteLine("All checks passed!");
+        }
+    }
+}
--- SSLHelper.cs
+++ SSLHelper.cs
@@ -6,16 +6,14 @@
 using System;
 using System.IO;
 using System.Linq;
-using System.Net;
 using System.Net.Security;
-using System.Net.Sockets;
 using System.Security.Cryptography.X509Certificates;
 
 namespace SSLHelper
 {
-    class Program
+    public static class SSLHelper
     {
-        static bool OnRemoteCertificateReceived(object sender, X509Certificate certificate, X509Chain chain, SslPolicyErrors sslPolicyErrors)
+        public static bool OnRemoteCertificateReceived(object sender, X509Certificate certificate, X509Chain chain, SslPolicyErrors sslPolicyErrors)
         {
             Console.WriteLine("Remote Certificate Received!");
             SslStream stream = (SslStream)sender;
@@ -65,7 +63,7 @@ namespace SSLHelper
             return ok;
         }
 
-        static X509Certificate OnClientCertificateRequested(
+        public static X509Certificate OnClientCertificateRequested(
             object sender, 
             string targetHost, 
             X509CertificateCollection localCertificates, 
@@ -76,63 +74,5 @@ namespace SSLHelper
             return null;
         }
 
-        static void Main(string[] args)
-        {
-            if (args.Length == 0)
-            {
-                Console.WriteLine("Usage: SSLHelper [hostname] [port]");
-                Console.WriteLine("port defaults to 443 if not specified");
-                return;
-            }
-
-            string hostname = args[0];
-            int port = args.Length == 1 ? 443 : int.Parse(args[1]);
-
-            var addr = Dns.GetHostAddresses(hostname).First();
-            Console.WriteLine($"DNS resolved {hostname} to address {addr}");
-            IPEndPoint remoteEP = new IPEndPoint(addr, port);
-
-            Console.WriteLine($"Creating TCP Socket and NetworkStream");
-            Socket s;
-            Stream tcpStream;
-            try
-            {
-                s = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
-                s.Connect(remoteEP);
-                tcpStream = new NetworkStream(s, ownsSocket: true);
-            }
-            catch (Exception e)
-            {
-                Console.WriteLine($"Network Stream creation failed with {e.GetType()}: {e.ToString()}");
-                return;
-            }
-
-            using (SslStream sslStream = new SslStream(tcpStream, false, OnRemoteCertificateReceived, OnClientCertificateRequested, EncryptionPolicy.RequireEncryption))
-            {
-                try
-                {
-                    sslStream.AuthenticateAsClient(hostname);
-
-                    Console.WriteLine($"TransportContext: {sslStream.TransportContext}");
-                    Console.WriteLine($"Protocol: {sslStream.SslProtocol}");
-
-                    Console.WriteLine($"KeyExchangeAlgorithm: {sslStream.KeyExchangeAlgorithm}");
-                    Console.WriteLine($"KeyExchangeStrength: {sslStream.KeyExchangeStrength}");
-
-                    Console.WriteLine($"CipherAlgorithm: {sslStream.CipherAlgorithm}");
-                    Console.WriteLine($"CipherStrength: {sslStream.CipherStrength}");
-
-                    Console.WriteLine($"HashAlgorithm: {sslStream.HashAlgorithm}");
-                    Console.WriteLine($"HashStrength: {sslStream.HashStrength}");
-                }
-                catch (Exception e)
-                {
-                    Console.WriteLine($"SSL Stream creation failed with {e.GetType()}: {e.ToString()}");
-                    return;
-                }
-            }
-
-            Console.WriteLine("All checks passed!");
-        }
     }
 }
